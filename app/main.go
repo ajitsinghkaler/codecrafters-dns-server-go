a320e7d16ef9e6d8aa6fffea078b583165fff234
Ajit Singh <ajitsinghkaler0@gmail.com>
2026-02-09 13:22:46 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"net"
 )
 
 // Ensures gofmt doesn't remove the "net" import in stage 1 (feel free to remove this!)
-var _ = net.ListenUDP
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -42,6 +43,64 @@ func main() {
 		// Create an empty response
 		response := []byte{}
 
+		writeBuf := new(bytes.Buffer)
+
+		var num uint16 = 1234
+
+		err = binary.Write(writeBuf, binary.BigEndian, num)
+
+		if err != nil {
+			fmt.Println("Error converting id to bytes1")
+		}
+
+		var headerVal uint8 = 1 << 7
+
+		err = binary.Write(writeBuf, binary.BigEndian, headerVal)
+
+		if err != nil {
+			fmt.Println("Error converting id to bytes2")
+		}
+
+		headerVal = 0
+
+		err = binary.Write(writeBuf, binary.BigEndian, headerVal)
+
+		if err != nil {
+			fmt.Println("Error converting id to bytes3")
+		}
+
+		var qdcount uint16 = 0
+
+		err = binary.Write(writeBuf, binary.BigEndian, qdcount)
+
+		if err != nil {
+			fmt.Println("Error converting id to bytes4")
+		}
+
+		var ancount uint16 = 0
+
+		err = binary.Write(writeBuf, binary.BigEndian, ancount)
+
+		if err != nil {
+			fmt.Println("Error converting id to bytes5")
+		}
+
+		var nscount uint16 = 0
+
+		err = binary.Write(writeBuf, binary.BigEndian, nscount)
+
+		if err != nil {
+			fmt.Println("Error converting id to bytes6")
+		}
+		var arcount uint16 = 0
+
+		err = binary.Write(writeBuf, binary.BigEndian, arcount)
+
+		if err != nil {
+			fmt.Println("Error converting id to bytes7")
+		}
+
+		response = append(response, writeBuf.Bytes()...)
 		_, err = udpConn.WriteToUDP(response, source)
 		if err != nil {
 			fmt.Println("Failed to send response:", err)
