d8ff88b0dbcc7abc37f2b1452279159553e82933
Ajit Singh <ajitsinghkaler0@gmail.com>
2026-02-09 13:26:17 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,29 +1,28 @@
 package main
 
 import (
-	"bytes"
 	"encoding/binary"
 	"fmt"
 	"net"
 )
 
-// Ensures gofmt doesn't remove the "net" import in stage 1 (feel free to remove this!)
+type DNSHeader struct {
+	ID      uint16
+	Flags   uint16
+	QDCount uint16
+	ANCount uint16
+	NSCount uint16
+	ARCount uint16
+}
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// TODO: Uncomment the code below to pass the first stage
-	//
 	udpAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:2053")
 	if err != nil {
-		fmt.Println("Failed to resolve UDP address:", err)
 		return
 	}
 
 	udpConn, err := net.ListenUDP("udp", udpAddr)
 	if err != nil {
-		fmt.Println("Failed to bind to address:", err)
 		return
 	}
 	defer udpConn.Close()
@@ -33,77 +32,46 @@ func main() {
 	for {
 		size, source, err := udpConn.ReadFromUDP(buf)
 		if err != nil {
-			fmt.Println("Error receiving data:", err)
 			break
 		}
 
-		receivedData := string(buf[:size])
-		fmt.Printf("Received %d bytes from %s: %s\n", size, source, receivedData)
-
-		// Create an empty response
-		response := []byte{}
-
-		writeBuf := new(bytes.Buffer)
-
-		var num uint16 = 1234
-
-		err = binary.Write(writeBuf, binary.BigEndian, num)
-
-		if err != nil {
-			fmt.Println("Error converting id to bytes1")
+		if size < 12 {
+			continue
 		}
 
-		var headerVal uint8 = 1 << 7
+		requestID := binary.BigEndian.Uint16(buf[0:2])
 
-		err = binary.Write(writeBuf, binary.BigEndian, headerVal)
+		var qr uint16 = 1
+		var opcode uint16 = 0
+		var aa uint16 = 0
+		var tc uint16 = 0
+		var rd uint16 = 0
+		var ra uint16 = 0
+		var z uint16 = 0
+		var rcode uint16 = 0
 
-		if err != nil {
-			fmt.Println("Error converting id to bytes2")
+		flags := (qr << 15) | (opcode << 11) | (aa << 10) | (tc << 9) | (rd << 8) | (ra << 7) | (z << 4) | rcode
+
+		header := DNSHeader{
+			ID:      requestID,
+			Flags:   flags,
+			QDCount: 0,
+			ANCount: 0,
+			NSCount: 0,
+			ARCount: 0,
 		}
 
-		headerVal = 0
+		response := make([]byte, 12)
+		binary.BigEndian.PutUint16(response[0:2], header.ID)
+		binary.BigEndian.PutUint16(response[2:4], header.Flags)
+		binary.BigEndian.PutUint16(response[4:6], header.QDCount)
+		binary.BigEndian.PutUint16(response[6:8], header.ANCount)
+		binary.BigEndian.PutUint16(response[8:10], header.NSCount)
+		binary.BigEndian.PutUint16(response[10:12], header.ARCount)
 
-		err = binary.Write(writeBuf, binary.BigEndian, headerVal)
-
-		if err != nil {
-			fmt.Println("Error converting id to bytes3")
-		}
-
-		var qdcount uint16 = 0
-
-		err = binary.Write(writeBuf, binary.BigEndian, qdcount)
-
-		if err != nil {
-			fmt.Println("Error converting id to bytes4")
-		}
-
-		var ancount uint16 = 0
-
-		err = binary.Write(writeBuf, binary.BigEndian, ancount)
-
-		if err != nil {
-			fmt.Println("Error converting id to bytes5")
-		}
-
-		var nscount uint16 = 0
-
-		err = binary.Write(writeBuf, binary.BigEndian, nscount)
-
-		if err != nil {
-			fmt.Println("Error converting id to bytes6")
-		}
-		var arcount uint16 = 0
-
-		err = binary.Write(writeBuf, binary.BigEndian, arcount)
-
-		if err != nil {
-			fmt.Println("Error converting id to bytes7")
-		}
-
-		response = append(response, writeBuf.Bytes()...)
 		_, err = udpConn.WriteToUDP(response, source)
 		if err != nil {
-			fmt.Println("Failed to send response:", err)
+			fmt.Printf("Error: %v\n", err)
 		}
 	}
 }
