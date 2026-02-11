fb689e23eb2e5088f22dc8f2fbc1c31d61cf7039
Ajit Singh <ajitsinghkaler0@gmail.com>
2026-02-11 12:05:18 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -2,117 +2,160 @@ package main
 
 import (
 	"encoding/binary"
+	"log"
 	"net"
 )
 
+const (
+	DNSPort       = "127.0.0.1:2053"
+	HeaderSize    = 12
+	TypeA         = 1
+	ClassInternet = 1
+)
+
+type DNSHeader struct {
+	ID      uint16
+	Flags   uint16
+	QDCount uint16
+	ANCount uint16
+}
+
+type DNSQuestion struct {
+	Name  []byte
+	Type  uint16
+	Class uint16
+}
+
 func main() {
-	udpAddr, _ := net.ResolveUDPAddr("udp", "127.0.0.1:2053")
-	udpConn, _ := net.ListenUDP("udp", udpAddr)
-	defer udpConn.Close()
+	conn, err := net.ListenUDP("udp", &net.UDPAddr{Port: 2053})
+	if err != nil {
+		log.Fatalf("Socket error: %v", err)
+	}
+	defer conn.Close()
 
-	buf := make([]byte, 512)
-
+	buffer := make([]byte, 512)
 	for {
-		size, source, err := udpConn.ReadFromUDP(buf)
-		if err != nil {
-			break
-		}
-
-		if size < 12 {
+		size, remoteAddr, _ := conn.ReadFromUDP(buffer)
+		if size < HeaderSize {
 			continue
 		}
 
-		requestID := binary.BigEndian.Uint16(buf[0:2])
-		qdCount := binary.BigEndian.Uint16(buf[4:6])
-		byte2 := buf[2]
-		opcode := (byte2 >> 3) & 0x0F
-		rd := byte2 & 0x01
-		var rcode byte = 0
-		if opcode != 0 {
-			rcode = 4
-		}
-
-		flags := uint16(1)<<15 | uint16(opcode)<<11 | uint16(rd)<<8 | uint16(rcode)
-
-		response := make([]byte, 12)
-		binary.BigEndian.PutUint16(response[0:2], requestID)
-		binary.BigEndian.PutUint16(response[2:4], flags)
-		binary.BigEndian.PutUint16(response[4:6], qdCount)
-		binary.BigEndian.PutUint16(response[6:8], qdCount)
-
-		currentOffset := 12
-		var names [][]byte
-
-		for i := 0; i < int(qdCount); i++ {
-			name, nextOffset := parseName(buf, currentOffset)
-			names = append(names, name)
-
-			response = append(response, name...)
-			qTypeClass := make([]byte, 4)
-			binary.BigEndian.PutUint16(qTypeClass[0:2], 1)
-			binary.BigEndian.PutUint16(qTypeClass[2:4], 1)
-			response = append(response, qTypeClass...)
-
-			currentOffset = nextOffset + 4
-		}
-
-		for _, name := range names {
-			answer := createAnswer(name, "8.8.8.8")
-			response = append(response, answer...)
-		}
-
-		udpConn.WriteToUDP(response, source)
+		response := processPacket(buffer[:size])
+		conn.WriteToUDP(response, remoteAddr)
 	}
 }
 
+func processPacket(rawRequest []byte) []byte {
+	header := parseHeader(rawRequest)
+	header.Flags = buildResponseFlags(rawRequest[2])
+	header.ANCount = header.QDCount
+
+	response := serializeHeader(header)
+	questions, _ := parseAllQuestions(rawRequest, int(header.QDCount))
+
+	for _, q := range questions {
+		response = append(response, encodeQuestion(q)...)
+	}
+
+	for _, q := range questions {
+		answer := buildARecord(q.Name, "8.8.8.8")
+		response = append(response, answer...)
+	}
+
+	return response
+}
+
+func parseHeader(data []byte) DNSHeader {
+	return DNSHeader{
+		ID:      binary.BigEndian.Uint16(data[0:2]),
+		QDCount: binary.BigEndian.Uint16(data[4:6]),
+	}
+}
+
+func buildResponseFlags(requestByte2 byte) uint16 {
+	opcode := (requestByte2 >> 3) & 0x0F
+	rd := requestByte2 & 0x01
+	rcode := uint16(0)
+	if opcode != 0 {
+		rcode = 4
+	}
+	return uint16(1)<<15 | uint16(opcode)<<11 | uint16(rd)<<8 | rcode
+}
+
+func serializeHeader(h DNSHeader) []byte {
+	buf := make([]byte, HeaderSize)
+	binary.BigEndian.PutUint16(buf[0:2], h.ID)
+	binary.BigEndian.PutUint16(buf[2:4], h.Flags)
+	binary.BigEndian.PutUint16(buf[4:6], h.QDCount)
+	binary.BigEndian.PutUint16(buf[6:8], h.ANCount)
+	return buf
+}
+
+func parseAllQuestions(data []byte, count int) ([]DNSQuestion, int) {
+	offset := HeaderSize
+	questions := make([]DNSQuestion, 0, count)
+
+	for i := 0; i < count; i++ {
+		name, nextOffset := parseName(data, offset)
+		q := DNSQuestion{
+			Name:  name,
+			Type:  binary.BigEndian.Uint16(data[nextOffset : nextOffset+2]),
+			Class: binary.BigEndian.Uint16(data[nextOffset+2 : nextOffset+4]),
+		}
+		questions = append(questions, q)
+		offset = nextOffset + 4
+	}
+	return questions, offset
+}
+
+func encodeQuestion(q DNSQuestion) []byte {
+	buf := append([]byte{}, q.Name...)
+	meta := make([]byte, 4)
+	binary.BigEndian.PutUint16(meta[0:2], q.Type)
+	binary.BigEndian.PutUint16(meta[2:4], q.Class)
+	return append(buf, meta...)
+}
+
+func buildARecord(name []byte, ip string) []byte {
+	record := append([]byte{}, name...)
+
+	meta := make([]byte, 10)
+	binary.BigEndian.PutUint16(meta[0:2], TypeA)
+	binary.BigEndian.PutUint16(meta[2:4], ClassInternet)
+	binary.BigEndian.PutUint32(meta[4:8], 60)
+	binary.BigEndian.PutUint16(meta[8:10], 4)
+
+	record = append(record, meta...)
+	record = append(record, net.ParseIP(ip).To4()...)
+	return record
+}
+
 func parseName(packet []byte, offset int) ([]byte, int) {
+	curr := offset
 	var name []byte
 	ptrFound := false
-	nextOffsetAfterName := 0
-	curr := offset
+	endOffset := 0
 
 	for {
-		if curr >= len(packet) {
-			break
-		}
 		b := packet[curr]
 		if b == 0 {
 			name = append(name, 0)
 			if !ptrFound {
-				nextOffsetAfterName = curr + 1
+				endOffset = curr + 1
 			}
 			break
 		}
-
 		if b&0xC0 == 0xC0 {
 			if !ptrFound {
-				nextOffsetAfterName = curr + 2
+				endOffset = curr + 2
 				ptrFound = true
 			}
-			ptr := int(binary.BigEndian.Uint16(packet[curr:curr+2]) & 0x3FFF)
-			curr = ptr
+			curr = int(binary.BigEndian.Uint16(packet[curr:curr+2]) & 0x3FFF)
 		} else {
 			length := int(b)
 			name = append(name, packet[curr:curr+length+1]...)
 			curr += length + 1
 		}
 	}
-
-	return name, nextOffsetAfterName
-}
-
-func createAnswer(name []byte, ip string) []byte {
-	res := append([]byte{}, name...)
-
-	meta := make([]byte, 10)
-	binary.BigEndian.PutUint16(meta[0:2], 1)
-	binary.BigEndian.PutUint16(meta[2:4], 1)
-	binary.BigEndian.PutUint32(meta[4:8], 60)
-	binary.BigEndian.PutUint16(meta[8:10], 4)
-	res = append(res, meta...)
-
-	parsedIP := net.ParseIP(ip).To4()
-	res = append(res, parsedIP...)
-
-	return res
+	return name, endOffset
 }
